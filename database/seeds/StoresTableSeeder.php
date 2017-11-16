32a0cd3de5b1750f7ab7aab8b2620afe766d89ba
parksm13 <parksm13@students.ecu.edu>
2017-11-16 18:52:06 -0500

    Added entries to seeder for Store table

--- database/seeds/StoresTableSeeder.php
+++ database/seeds/StoresTableSeeder.php
@@ -11,6 +11,22 @@ class StoresTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('stores')->insert([
+	    'name' => 'Store 1',
+	    'address' => '123 Main St.',
+	    'location' => 'GPS Coordinates'
+	]);
+
+        DB::table('stores')->insert([
+	    'name' => 'Store 2',
+	    'address' => '456 Main St.',
+	    'location' => 'GPS Coordinates'
+	]);
+
+        DB::table('stores')->insert([
+	    'name' => 'Store 3',
+	    'address' => '789 Main St.',
+	    'location' => 'GPS Coordinates'
+	]);
     }
 }
