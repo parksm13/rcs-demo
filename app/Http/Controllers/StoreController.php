fbd472cbe5d9dd42108eb5c205356f5484e3f925
parksm13 <parksm13@students.ecu.edu>
2017-11-16 19:06:05 -0500

    Added database results to index method in Store Controller

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Store;
+
 class StoreController extends Controller
 {
     public function index() {
-        return view('store');
+        $stores = Store::all();
+        return view('store', compact('stores'));
     }
 }
