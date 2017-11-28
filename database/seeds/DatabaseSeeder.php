ff195ab84aa1e58497cdf087d055c3bc5f3a9b98
Alan Master <master9559@yahoo.com>
2017-11-27 20:25:40 -0500

    migrating new users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
 		$this->call(StoresTableSeeder::class);
 		$this->call(ItemsTableSeeder::class);
 		$this->call(UsersTableSeeder::class);
+		//$this->call(PaymentMethodTableSeeeder::class);
     }
 }
---
+++ database/seeds/PaymentMethodTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PaymentMethodTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		$Testerid1 = DB::table("users")->select("id")->where("username","Tester1")->get();
+		
+        DB::table('payment_method')->insert([
+	    	"type" => "moneyCard",
+			"card_number" => "### Card Number ###",
+			"security_code" => "000",
+			"expiration_date" => "2017-11-11",
+			"creation_date" => "2017-11-11",
+			"owner_id" => $Testerid1,
+		]);
+    }
+}
