2291a99332a55f9697ea830f78fef00379443808
parksm13 <parksm13@students.ecu.edu>
2017-11-28 16:10:05 -0500

    Added List controller and updated List model

--- app/List.php
+++ app/List.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class List extends Model
 {
-    //
+    public function getUserLists($id) {
+	$lists = $table('lists')->where('owner_id', $id)->get();
+	return $lists;
+    };
 }
