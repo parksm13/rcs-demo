54bd03770822a5958d0e7bf975223f0c04c5c8aa
James Adams <jadams27@elon.edu>
2017-11-28 19:19:59 -0500

    Added text to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
-<p> About view </p>
+<p>Retail Convenience Solutions is a web application developed by Group 3 for SENG 6240 at East Carolina University.</p>
+
+<p>This application provides a convenient method to find nearby stores and browse items contained in each store.
+Users can also add and remove items from a shopping list.</p>
+
+<p>Group 3 Team members:</p>
+
+<ul>
+  <li>James Adams</li>
+  <li>Anil Adhikari</li>
+  <li>Arjun Aneja</li>
+  <li>Alan Master</li>
+  <li>Michael Parks</li>
+</ul>  
 
 @endsection
